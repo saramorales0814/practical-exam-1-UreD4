e4c548001fbaabb960b96a360b29d1141b92b554
UreD4 <ttroque@miners.utep.edu>
2020-10-11 14:53:36 -0600

    Update p3_hex.h

--- p3_hex.h
+++ p3_hex.h
@@ -4,13 +4,27 @@
 *   The programmer must write code that prints out the decimal value
 *   of a numerical character, as long as the character is
 *   a valid Hexadecimal number
-*   Last modified by: Angel F. Garcia (9/30/2020)
+*   Last modified by: Thomas T Roque (10/11/2020)
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 void main_hex(void) {
-  char hex_in;
-  scanf("%c", &hex_in);
-  
+  char hex_in[1];
+  int intValue;
+  //Scanf was being skipped with %s
+  scanf("%s", &hex_in[0]);
   //TODO: Your code goes here
+  if(hex_in[0]=='A'||'a'){printf("10");}
+  else if (hex_in[0]=='B'||'B'){printf("11");}
+  else if (hex_in[0]=='C'||'c'){printf("12");}
+  else if (hex_in[0]=='D'||'d'){printf("13");}
+  else if (hex_in[0]=='E'||'e'){printf("14");}
+  else if (hex_in[0]=='F'||'f'){printf("15");}
+  else {sscanf(hex_in[0],"%d", &intValue);}
+  if(intValue<9){printf(intValue);}
+  else printf("Please enter a value 0-9 or A-F\n");
+
+
+
 }
