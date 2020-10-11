4e6094e1b810b646bb24a7a205b536fea3a152a8
UreD4 <ttroque@miners.utep.edu>
2020-10-11 15:36:05 -0600

    Update p3_hex.h
    Corrected to a working model

--- p3_hex.h
+++ p3_hex.h
@@ -15,16 +15,17 @@ void main_hex(void) {
   //Scanf was being skipped with %s
   scanf("%s", &hex_in[0]);
   //TODO: Your code goes here
-  if(hex_in[0]=='A'||'a'){printf("10");}
-  else if (hex_in[0]=='B'||'B'){printf("11");}
-  else if (hex_in[0]=='C'||'c'){printf("12");}
-  else if (hex_in[0]=='D'||'d'){printf("13");}
-  else if (hex_in[0]=='E'||'e'){printf("14");}
-  else if (hex_in[0]=='F'||'f'){printf("15");}
-  else {sscanf(hex_in[0],"%d", &intValue);}
-  if(intValue<9){printf(intValue);}
-  else printf("Please enter a value 0-9 or A-F\n");
-
-
-
+  if(hex_in[0]=='A'||hex_in[0]=='a'){printf("10\n");}
+  else if (hex_in[0]=='B'||hex_in[0]=='B'){printf("11\n");}
+  else if (hex_in[0]=='C'||hex_in[0]=='c'){printf("12\n");}
+  else if (hex_in[0]=='D'||hex_in[0]=='d'){printf("13\n");}
+  else if (hex_in[0]=='E'||hex_in[0]=='e'){printf("14\n");}
+  else if (hex_in[0]=='F'||hex_in[0]=='f'){printf("15\n");}
+  //converts string to int
+  else {
+    intValue = atoi(hex_in);
+  //prints anser or error to user
+    if(intValue<9){printf("%d\n",intValue);}
+    else printf("Please enter a value 0-9 or A-F\n");
+  }
 }
