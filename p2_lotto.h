54548cfb0b61fa2c7513e0b78778d7dddbd9fc61
UreD4 <ttroque@miners.utep.edu>
2020-10-11 14:55:14 -0600

    Update p2_lotto.h

--- p2_lotto.h
+++ p2_lotto.h
@@ -9,11 +9,24 @@
 #include <stdio.h>
 
 void main_lotto(void) {
-  char winner[5] = "7239";
-  char ticket[5];
+  char winner[4] = "7239";
+  char ticket[4];
 
   scanf("%s", ticket);
 
   //TODO: Your code goes here
-  
+  int i=0;
+  int count=0;
+  //Avoiding any issues if the user inputs an empty string
+  while(ticket!=NULL && i<4){
+    if(ticket[i]==winner[i]){count+=1;}
+    i++;
+  }
+
+  //Switch statement made in if cases
+  if (count==4){printf("1st Place winner");}
+  else if (count==3){printf("2nd Place winner");}
+  else if (count==2){printf("3rd Place winner");}
+  else printf("Better luck next time!");
+
 }
