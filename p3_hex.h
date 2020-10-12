1402f7f9eb7e7a238cbfcef246c3cfbc03b0aee2
saramorales0814 <72719912+saramorales0814@users.noreply.github.com>
2020-10-11 21:17:34 -0600

    Update p3_hex.h

--- p3_hex.h
+++ p3_hex.h
@@ -16,11 +16,11 @@ void main_hex(void) {
   scanf("%s", &hex_in[0]);
   //TODO: Your code goes here
   if(hex_in[0]=='A'||hex_in[0]=='a'){printf("10\n");}
-  else if (hex_in[0]=='B'||hex_in[0]=='B'){printf("11\n");}
-  else if (hex_in[0]=='C'||hex_in[0]=='c'){printf("12\n");}
-  else if (hex_in[0]=='D'||hex_in[0]=='d'){printf("13\n");}
-  else if (hex_in[0]=='E'||hex_in[0]=='e'){printf("14\n");}
-  else if (hex_in[0]=='F'||hex_in[0]=='f'){printf("15\n");}
+  else if (hex_in[0]=='b'||hex_in[0]=='B'){printf("11\n");}
+  else if (hex_in[0]=='c'||hex_in[0]=='C'){printf("12\n");}
+  else if (hex_in[0]=='d'||hex_in[0]=='D'){printf("13\n");}
+  else if (hex_in[0]=='e'||hex_in[0]=='E'){printf("14\n");}
+  else if (hex_in[0]=='f'||hex_in[0]=='F'){printf("15\n");}
   //converts string to int
   else {
     intValue = atoi(hex_in);
